c8ed5b1313c00d4078e633a9ca22f9f574551e6c
Midhun <midhun.t@webandcrafts.in>
2025-09-18 13:27:08 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from "@tailwindcss/vite";
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(),],
+// });
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
-
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  
+  plugins: [react(),tailwindcss()],
+  server: {port: 5174},
+  alias: {
+    '@': '/src', // Ensure this points to your src directory
+  },
 });
