dcd0e863f650bdbaa4a9652fd593c8462448e1da
Midhun <midhun.t@webandcrafts.in>
2025-09-18 14:05:43 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes, BrowserRouter, useNavigate } from "react-router-dom";
-import { useState } from "react"; // Import useState for managing the mode
+import { useState } from "react"; 
 import Header from "./Components/Header";
 import WidgetEditor from "./Pages/WidgetEditor";
 import Preview from "./Components/Preview";
@@ -8,22 +8,22 @@ import { Toaster } from "sonner";
 export default function App() {
   return (
     <BrowserRouter>
-      <MainApp /> {/* This is where the main logic and routing should happen */}
+      <MainApp /> 
     </BrowserRouter>
   );
 }
 
 function MainApp() {
   const [isPreviewMode, setIsPreviewMode] = useState(false);
-  const navigate = useNavigate(); // Now it's within the Router context
+  const navigate = useNavigate(); 
 
   const handleToggleMode = (mode) => {
     if (mode === "preview") {
       setIsPreviewMode(true);
-      navigate("/preview"); // Navigate to the preview page
+      navigate("/preview"); 
     } else {
       setIsPreviewMode(false);
-      navigate("/"); // Navigate back to the home (edit) page
+      navigate("/"); 
     }
   };
 
